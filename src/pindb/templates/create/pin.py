968cb2ee2ad06020ac7b50eaa04528549148e3a6
Josh Maddy <joshuadcmaddy@gmail.com>
2025-08-16 20:36:04 -0400

    Pin form improvements

--- src/pindb/templates/create/pin.py
+++ src/pindb/templates/create/pin.py
@@ -22,11 +22,21 @@ def pin_form(
                 h2["Required Fields"],
                 div[
                     label(for_="name")["Name"],
-                    input(type="text", name="name", id="name", required=True),
+                    input(
+                        type="text",
+                        name="name",
+                        id="name",
+                        required=True,
+                        autocomplete="off",
+                    ),
                 ],
                 div[
                     label(for_="acquisition_type")["Acquisition"],
-                    select(name="acquisition_type", required=True)[
+                    select(
+                        name="acquisition_type",
+                        id="acquisition_type",
+                        required=True,
+                    )[
                         [
                             option(value=acquisition_type)[
                                 acquisition_type.replace("_", " ").title()
@@ -37,7 +47,11 @@ def pin_form(
                 ],
                 div[
                     label(for_="material_ids")["Materials"],
-                    select(name="material_ids", required=True)[
+                    select(
+                        name="material_ids",
+                        id="material_ids",
+                        required=True,
+                    )[
                         [
                             option(value=material.id)[material.name]
                             for material in materials
@@ -46,31 +60,54 @@ def pin_form(
                 ],
                 div[
                     label(for_="shop_ids")["Shops"],
-                    select(name="shop_ids", required=True)[
-                        [option(value=shop.id)[shop.name] for shop in shops]
-                    ],
+                    select(
+                        name="shop_ids",
+                        id="shop_ids",
+                        required=True,
+                    )[[option(value=shop.id)[shop.name] for shop in shops]],
                 ],
                 h2["Optional Fields"],
                 # Production
                 div[
                     label(for_="limited_edition")["Limited Edition"],
-                    input(name="limited_edition", type="checkbox"),
+                    input(
+                        name="limited_edition",
+                        id="limited_edition",
+                        type="checkbox",
+                    ),
                 ],
                 div[
                     label(for_="number_produced")["Number Produced"],
-                    input(name="number_produced", type="number", min=0, step=1),
+                    input(
+                        name="number_produced",
+                        id="number_produced",
+                        type="number",
+                        min=0,
+                        step=1,
+                    ),
                 ],
                 div[
                     label(for_="release_date")["Release Date"],
-                    input(name="release_date", type="date"),
+                    input(
+                        name="release_date",
+                        id="release_date",
+                        type="date",
+                    ),
                 ],
                 div[
                     label(for_="end_date")["End Date"],
-                    input(name="end_date", type="date"),
+                    input(
+                        name="end_date",
+                        id="end_date",
+                        type="date",
+                    ),
                 ],
                 div[
                     label(for_="funding_type")["Funding Type"],
-                    select(name="funding_type")[
+                    select(
+                        name="funding_type",
+                        id="funding_type",
+                    )[
                         [
                             option(value=funding)[funding.replace("_", " ").title()]
                             for funding in FundingType
@@ -80,15 +117,32 @@ def pin_form(
                 # Physical
                 div[
                     label(for_="posts")["Posts"],
-                    input(name="posts", type="number", min=1, step=1, value=1),
+                    input(
+                        name="posts",
+                        id="posts",
+                        type="number",
+                        min=1,
+                        step=1,
+                        value=1,
+                    ),
                 ],
                 div[
                     label(for_="width")["Width"],
-                    input(name="width", type="text", pattern=DIMENSION_PATTERN),
+                    input(
+                        name="width",
+                        id="width",
+                        type="text",
+                        pattern=DIMENSION_PATTERN,
+                    ),
                 ],
                 div[
                     label(for_="height")["Height"],
-                    input(name="height", type="text", pattern=DIMENSION_PATTERN),
+                    input(
+                        name="height",
+                        id="height",
+                        type="text",
+                        pattern=DIMENSION_PATTERN,
+                    ),
                 ],
                 input(type="submit", value="Submit"),
             ],
