c33eb961fde8f942ce53127da10c4d570913659f
Josh Maddy <joshuadcmaddy@gmail.com>
2026-04-18 11:27:53 -0400

    Again

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,4 @@ echo "Running database migrations..."
 uv run alembic upgrade head
 
 echo "Starting server..."
-exec uv run fastapi run ./src/pindb/ --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips='*'
+exec uv run uvicorn pindb:app --host 0.0.0.0 --port 8000 --proxy-headers --forwarded-allow-ips='*'
